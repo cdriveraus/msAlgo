da809d83c69367bf69fc60ca642424d0bce2d4f3
Charles Driver <cdriver.aus@gmail.com>
2021-08-02 15:43:25 +0200

    init

---
+++ R/algoselect.R
@@ -0,0 +1,8 @@
+
+# itemcode <- bigIRT:::selectItem(items[Scale %in% scale & !Item %in% adat2$item,],
+#   ability = unlist(persons[person,scale,with=FALSE]),
+#   targetease = 0.1,samplesize = 1)
+
+selectItem <- function(items, scalename,ability, targetease=.1, samplesize=1){
+  sample(items[Scale %in% scalename,]$Item[order(abs(ability-items$B-targetease))[1:min(nrow(items),samplesize)]],size = 1) #could use expected information instead
+}
---
+++ R/algoserver.R
@@ -0,0 +1,6 @@
+#
+# require(bigIRT);
+# setwd("/home/driver/bigIRT/testing/")
+# items <- read.csv(file = 'items.csv')
+# persons = read.csv(file='persons.csv')
+# while (!exists("done")) Sys.sleep(1)
