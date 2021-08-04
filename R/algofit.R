49e036eac9d3d17d245f06b3fcb8ba8519d0eaa4
Charles Driver <cdriver.aus@gmail.com>
2021-08-04 14:50:39 +0200

    .

--- R/algofit.R
+++ R/algofit.R
@@ -1,17 +1,27 @@
+msFit <- function(jsontextdat){
+  adat=as.data.table(jsonlite::fromJSON(jsontextdat))
+  Ability=bigIRT::fitIRT(
+    dat = adat,
+    score='score',id = 'id',
+    item = 'item',scale = 'Scale',pl = 2,
+    cores=1,  priors = TRUE,ebayes = FALSE,itemDat = adat,
+    normalise = FALSE,dropPerfectScores = FALSE)$pars$Ability
+}
+
 # setwd("/home/driver/bigIRT/testing/")
 # load(file='adat.rda');
 # require(data.table)
-# require(bigIRT)
-# con <- socketConnection(host = "localhost", port = 8888, #connect to socket server
-#   blocking = FALSE, timeout = 3)
+# # require(bigIRT)
+# # con <- socketConnection(host = "localhost", port = 8888, #connect to socket server
+# #   blocking = FALSE, timeout = 3)
+# #
+# # svSocket::evalServer(con,adat2,as.data.frame(adat))
+# #
+# # Ability= svSocket::evalServer(con,
+# # Ability=bigIRT::fitIRT(dat = adat,score='score',id = 'id',item = 'item',scale = 'Scale',pl = 2,cores=1,  priors = TRUE,ebayes = FALSE,itemDat = adat2,normalise = FALSE,dropPerfectScores = FALSE)$pars$Ability
+# # )
 #
-# svSocket::evalServer(con,adat2,as.data.frame(adat))
+# # svSocket::evalServer(con, 'done <- NULL')
 #
-# Ability= svSocket::evalServer(con,
-# Ability=bigIRT::fitIRT(dat = adat,score='score',id = 'id',item = 'item',scale = 'Scale',pl = 2,cores=1,  priors = TRUE,ebayes = FALSE,itemDat = adat2,normalise = FALSE,dropPerfectScores = FALSE)$pars$Ability
-# )
-
-# svSocket::evalServer(con, 'done <- NULL')
-
-
-
+#
+#
